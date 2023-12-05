ee31ac3e903e83c3ab47a434cbea096be00036e0
Jaiharan <136414923+Jaiharan@users.noreply.github.com>
2023-12-05 23:23:55 +0530

    updated login and register

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegisterPage = () => {
+
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+
+  async function register(e){
+    e.preventDefault();
+    const response = await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type': 'application/json'}
+    });
+    if(response.status !== 200){
+      alert('Registration Failed')
+    }else{
+      alert('Registration Successfull')
+    }
+    
+  }
+
   return (
-    <form className="register ">
+    <form className="register " onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder='username'/>
-      <input type="password" placeholder='password'/>
+      <input type="text" 
+              placeholder='username' 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)}
+              />
+      <input type="password"
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
       <button>Register</button>
     </form>
   )
